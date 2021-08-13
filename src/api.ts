817f60d522bd7213bb8271d857b84bbb30b1b061
Paul Ogolla <paulotieno2@gmail.com>
2021-08-13 10:47:12 -0700

    add question state and handle api response

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,33 @@
+import { shuffleArray } from "./utils";
+
 export enum Difficulty {
-    EASY = 'easy',
-    MEDIUM = 'medium',
-    HARD = 'hard'
+  EASY = 'easy',
+  MEDIUM = 'medium',
+  HARD = 'hard'
 }
+
+export type Question = {
+  category: string;
+  difficulty: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+}
+
+export type QuestionState =  Question & { answers: string[]};
+
 export const fetchQuestions = async (amount: number, difficulty: Difficulty) => {
-    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
-    const data = await(await (await fetch(endpoint)).json());
-    console.log(data)
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const data = await(await (await fetch(endpoint)).json());
+  return data.results.map((question: Question) => (
+    {
+      ...question,
+      answers: shuffleArray([
+        question.incorrect_answers,
+        question.correct_answer,
+      ])
+    }
+  ))
+  
 }
